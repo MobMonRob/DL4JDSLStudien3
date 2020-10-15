60a8a57b76f78102118381f736160795ff1845b7
root <root@LAPTOP-26F1VE11.localdomain>
2020-10-15 12:51:47 +0200

    Changed line endings, hopefully

--- preprolanguage/component/make_component.sh
+++ preprolanguage/component/make_component.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 readonly JAVA_VERSION="${1}"
 if [[ $JAVA_VERSION == 1.8* ]]; then
@@ -35,8 +35,8 @@ mkdir -p "$COMPONENT_DIR/META-INF"
 {
     echo "Bundle-Name: PrePro";
     echo "Bundle-Symbolic-Name: com.oracle.truffle.sl";
-    echo "Bundle-Version: 20.0.0";
-    echo 'Bundle-RequireCapability: org.graalvm; filter:="(&(graalvm_version=20.0.0)(os_arch=amd64))"';
+    echo "Bundle-Version: 20.2.0";
+    echo 'Bundle-RequireCapability: org.graalvm; filter:="(&(graalvm_version=20.2.0)(os_arch=amd64))"';
     echo "x-GraalVM-Polyglot-Part: True"
 } > "$COMPONENT_DIR/META-INF/MANIFEST.MF"
 
--- preprolanguage/native/clean_native.sh
+++ preprolanguage/native/clean_native.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 [ -f prepronative ] && rm prepronative || echo "prepronative not found."
